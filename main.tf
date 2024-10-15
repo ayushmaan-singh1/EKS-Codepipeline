d90f3f99997b3a889c69539166872c75e410fcbe
root <root@master.example.com>
2024-10-15 18:45:15 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,58 @@
-# Configure the AWS Provider
+# Configuring the AWS Provider
 provider "aws" {
   version = "~> 5.0"
   region  = "ap-south-1"
 }
 
-# Create a VPC
+# Creating VPC
 resource "aws_vpc" "my-vpc" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "my-vpc"
+  }
 }
 
-#Creating Subnet
-resource "aws_subnet" "Public" {
-  vpc_id = aws_vpc.my-vpc.id
-  cidr_block = "10.0.1.0/24"
+# Creating Public Subnet
+resource "aws_subnet" "Public1" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "ap-south-1a"
- }
+  availability_zone       = "ap-south-1a"
+  tags = {
+    Name = "Public_subnet2"
+  }
+}
 
-resource "aws_subnet" "Private" {
-  vpc_id = aws_vpc.my-vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "ap-south-1b"  
+resource "aws_subnet" "Public2" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1c"
+  tags = {
+    Name = "Public_subnet2"
+  }
 }
 
 
-#Creating Internet Gateway
-  
+# Creating Private Subnet
+resource "aws_subnet" "Private" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "Private_subnet"
+  }
+}
+
+# Creating Internet Gateway
 resource "aws_internet_gateway" "my-igw" {
   vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "my-igw"
+  }
 }
 
-#Public Route-Table
+# Public Route Table
 resource "aws_route_table" "Public_RT" {
   vpc_id = aws_vpc.my-vpc.id
 
@@ -38,92 +60,174 @@ resource "aws_route_table" "Public_RT" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.my-igw.id
   }
+
+  tags = {
+    Name = "Public_RT"
+  }
 }
 
-#Associating Public subnet with Public_RT
-resource "aws_route_table_association" "public_subnet_assoc" {
-  subnet_id = aws_subnet.Public.id
+# Associating Public Subnet RT
+resource "aws_route_table_association" "public1_subnet_assoc" {
+  subnet_id      = aws_subnet.Public1.id
   route_table_id = aws_route_table.Public_RT.id
 }
 
-#Creating SG
+resource "aws_route_table_association" "public2_subnet_assoc" {
+  subnet_id      = aws_subnet.Public2.id
+  route_table_id = aws_route_table.Public_RT.id
+}
+
+# Creating Security Group to Allow SSH and All Traffic
 resource "aws_security_group" "my-SG" {
-  description = "Allowing ALL"
+  description = "Allowing all traffic for EKS"
   vpc_id      = aws_vpc.my-vpc.id
-  
+  # Allow all other traffic
   ingress {
-	from_port = 0
-	to_port   = 0
-	protocol  = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-   }
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
-        from_port = 0
-        to_port   = 0
-        protocol  = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my-SG"
   }
 }
-#EKS Cluster Creation
+
+
+# EKS Cluster
 resource "aws_eks_cluster" "Ayushmaan-EKS" {
-  name = "Ayushmaan-EKS"
-  role_arn = "arn:aws:iam::851725241695:role/Ayushmaan-EKS"
+  name     = "Ayushmaan-EKS"
+  role_arn = "arn:aws:iam::851725241695:role/Ayushmaan-EKS" 
 
   vpc_config {
-    subnet_ids = [aws_subnet.Public.id, aws_subnet.Private.id]
-    security_group_ids = [aws_security_group.my-SG.id]
+    subnet_ids             = [aws_subnet.Public1.id, aws_subnet.Public2.id]
+    security_group_ids     = [aws_security_group.my-SG.id]
+    endpoint_public_access = true
+    endpoint_private_access = false
+  }
+
+  tags = {
+    Name = "Ayushmaan-EKS"
   }
 }
-#EKS_Node_Group
+
+# Data source for EKS addon versions
+data "aws_eks_addon_version" "vpc_cni" {
+  addon_name         = "vpc-cni"
+  kubernetes_version = aws_eks_cluster.Ayushmaan-EKS.version
+  most_recent        = true
+}
+
+data "aws_eks_addon_version" "coredns" {
+  addon_name         = "coredns"
+  kubernetes_version = aws_eks_cluster.Ayushmaan-EKS.version
+  most_recent        = true
+}
+
+data "aws_eks_addon_version" "kube_proxy" {
+  addon_name         = "kube-proxy"
+  kubernetes_version = aws_eks_cluster.Ayushmaan-EKS.version
+  most_recent        = true
+}
+
+data "aws_eks_addon_version" "pod_identity" {
+  addon_name         = "eks-pod-identity"
+  kubernetes_version = aws_eks_cluster.Ayushmaan-EKS.version
+  most_recent        = true
+}
+
+# EKS Addon for VPC CNI
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name  = aws_eks_cluster.Ayushmaan-EKS.name
+  addon_name    = "vpc-cni"
+  addon_version = data.aws_eks_addon_version.vpc_cni.version
+}
+
+# EKS Addon for CoreDNS
+resource "aws_eks_addon" "coredns" {
+  cluster_name  = aws_eks_cluster.Ayushmaan-EKS.name
+  addon_name    = "coredns"
+  addon_version = data.aws_eks_addon_version.coredns.version
+}
+
+# EKS Addon for Kube Proxy
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name  = aws_eks_cluster.Ayushmaan-EKS.name
+  addon_name    = "kube-proxy"
+  addon_version = data.aws_eks_addon_version.kube_proxy.version
+}
+
+# EKS Addon for EKS Pod Identity
+resource "aws_eks_addon" "pod_identity" {
+  cluster_name  = aws_eks_cluster.Ayushmaan-EKS.name
+  addon_name    = "eks-pod-identity"
+  addon_version = data.aws_eks_addon_version.pod_identity.version
+}
+
+# EKS Node Group 
 resource "aws_eks_node_group" "Ayushmaan-NG" {
   cluster_name    = aws_eks_cluster.Ayushmaan-EKS.name
-  node_role_arn   = "arn:aws:iam::851725241695:role/EC2-EKS"
-  subnet_ids      = [aws_subnet.Public.id]
+  node_role_arn   = "arn:aws:iam::851725241695:role/EKSFull"  
+  subnet_ids      = [aws_subnet.Public1.id, aws_subnet.Public2.id]
 
   scaling_config {
     desired_size = 1
     max_size     = 1
     min_size     = 1
   }
-  
+  ami_type = "AL2_x86_64"
   instance_types = ["t2.medium"]
+  capacity_type = "ON_DEMAND"
+  disk_size = 20
+  
+  remote_access {
+  ec2_ssh_key = "server"
+  source_security_group_ids = [aws_security_group.my-SG.id]
+  }
+  tags = {
+    Name = "EKS-Node-Group"
+  }
 }
 
-#AWS_Instance
+# AWS Instance
 resource "aws_instance" "operating" {
-  ami           = "ami-0dee22c13ea7a9a67"
-  instance_type = "t2.medium"
-  key_name      = "server"
-  security_groups = [aws_security_group.my-SG.name]
-  iam_instance_profile = "arn:aws:iam::851725241695:instance-profile/EKSFull"
+  ami                 = "ami-0dee22c13ea7a9a67"
+  instance_type       = "t2.medium"
+  key_name            = "server" 
+  subnet_id           = aws_subnet.Public1.id
+  security_groups     = [aws_security_group.my-SG.name]
 
-  #USER-Data
+  iam_instance_profile = "arn:aws:iam::851725241695:instance-profile/EKSFull"  
+
+  # User Data
   user_data = <<-EOF
 	      #!/bin/bash
-      	      sudo apt -y update
-              sudo apt -y install docker.io openjdk-21* unzip
-              #AWSCLI
-              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-              unzip awscliv2.zip
-              sudo ./aws/install
-              # for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
-              ARCH=amd64
-              PLATFORM=$(uname -s)_$ARCH
-	      #EKSCTL
-              curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-
-              # (Optional) Verify checksum
-              curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
-
-              tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-
-              sudo mv /tmp/eksctl /usr/local/bin
-              #KUBECTL
-              curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-              sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-              chmod +x kubectl
-              mkdir -p ~/.local/bin
-              mv ./kubectl ~/.local/bin/kubectl
-              EOF
+	      sudo apt -y update
+	      sudo apt -y install docker.io openjdk-21* unzip
+	      # AWS CLI installation
+	      curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+	      unzip awscliv2.zip
+	      sudo ./aws/install
+	      # EKSCTL installation
+	      ARCH=amd64
+	      PLATFORM=$(uname -s)_$ARCH
+	      curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+	      tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+	      sudo mv /tmp/eksctl /usr/local/bin
+	      # KUBECTL installation
+	      curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+	      sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+	      EOF
+  
+  tags = {
+    Name = "operating"
+  }
 }
-
